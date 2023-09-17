114c5da0030239281866d3d743c858fbe92b3ba6
Jesper-dev <okword9@gmail.com>
2023-09-17 13:21:55 +0200

    Hamburger menu

--- src/layout/navbar/nav.tsx
+++ src/layout/navbar/nav.tsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
 import "./nav.less";
 
 export const Nav = () => {
-    return (
-        <nav>
+    const [openNav, setOpenNav] = useState(false);
+    const renderLinks = () => {
+        if (!openNav) return <></>;
+        return (
             <ul>
                 <li>
                     <Link to={""}>Init tracker</Link>
@@ -12,6 +18,20 @@ export const Nav = () => {
                     <Link to={"damage"}>Damage Tracker</Link>
                 </li>
             </ul>
+        );
+    };
+    return (
+        <nav>
+            <div className="container">
+                {renderLinks()}
+                <div className="toggle_icon_container">
+                    <FontAwesomeIcon
+                        onClick={() => setOpenNav(!openNav)}
+                        icon={faBars}
+                        className="toggle_icon"
+                    />
+                </div>
+            </div>
         </nav>
     );
 };
